1381f8f792b9f3653125ecaaa9d3d7af85502d2a
mishabartone <77360719+mishabartone@users.noreply.github.com>
2023-02-03 13:56:58 +0100

    Initial Commit

---
+++ GuessTheNumber/GuessTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheNumberApp.swift
+//  GuessTheNumber
+//
+//  Created by Михаил Железовский on 03.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
